2b334d641c7821d8e5cbb06591677f315d796478
vis52 <vs6773817@gmail.com>
2025-02-21 12:46:10 +0530

    a

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,10 +12,10 @@ const Contact = () => {
         
                 
            <a href='6398730468' className='icon-link'> <i class="bi bi-telephone-forward"> </i>    +91-6398730468</a>
-           <a href='' className='icon-link'> <i class="bi bi-envelope-at"></i>myselfvishal954@gmail.com </a>
-           <a href='https://www.linkedin.com/in/vishal-sharma-708833228/' className='icon-link'> <i class="bi bi-linkedin"></i>Vishal Sharma</a>
-            <a href='https://github.com/Vis52/' className='icon-link'><i class="bi bi-github"></i> Vishal Sharma</a>
-            <a href='http://instagram.com/vishal_sharma_0002/' className='icon-link'><i class="bi bi-instagram"></i>Vishal_Sharma_0002</a>
+           <a href='' className='icon-link'> <i class="bi bi-envelope-at"></i>   myselfvishal954@gmail.com </a>
+           <a href='https://www.linkedin.com/in/vishal-sharma-708833228/' className='icon-link'> <i class="bi bi-linkedin"></i>       Vishal Sharma</a>
+            <a href='https://github.com/Vis52/' className='icon-link'><i class="bi bi-github"></i>  Vishal Sharma</a>
+            <a href='http://instagram.com/vishal_sharma_0002/' className='icon-link'><i class="bi bi-instagram"></i>   Vishal_Sharma_0002</a>
             
 
        
